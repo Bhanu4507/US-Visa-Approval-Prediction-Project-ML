9c6ef416fc712382d100e70445aad1b73d066c27
Bhanu Prakash <bhanuprakashkalla.1995@gmail.com@example.com>
2025-04-27 00:00:57 +0530

    save

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name = "us_visa",
     version = "0.0.0",
-    author= "Bhanu Prakash"
-    author_mail = "bhanuprakashkalla.1995@gmail.com",
+    author= "Bhanu Prakash",
+    author_email = "bhanuprakashkalla.1995@gmail.com",
     packages = find_packages()
     
 )
